3791140d0a0b3badf371273243c02bff66d0d21f
ZaalTsagareli <zaali.tsagareli@makingscience.com>
2024-01-16 18:47:44 +0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import CountryDetails from "./pages/Country-details";
+import CountrySearch from "./pages/Country-search";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search" element={<CountrySearch />} />
+        <Route path="/details/:countryCode" element={<CountryDetails />} />
+      </Routes>
+    </Router>
   );
 }
 
